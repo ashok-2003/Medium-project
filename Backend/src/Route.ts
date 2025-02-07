b4392af568612bf9732bb7be79d946e2afb70d40
Ashok <ashokgupta2678@gmail.com>
2025-02-08 02:29:16 +0530

    added middware for authincated routes

--- Backend/src/Route.ts
+++ Backend/src/Route.ts
@@ -3,11 +3,14 @@ import { Hono } from "hono";
 const app = new Hono<{
     Bindings: {
         DATABASE_URL: string   // did this so that my prisma cliet will stop compalining 
+        JWT_SECRET : string
     }
-}>().basePath('api/v1'); // stored the api call for version 1 of my application
+}>(); // stored the api call for version 1 of my application
 
 import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
+import { sign, verify } from 'hono/jwt'
+
 
 
 
@@ -35,9 +38,22 @@ app.post('/signin', async (c) => {
         await prisma.$disconnect(); 
         // awaiting for disconnet to avaiod leak 
 
+        // so giving the web token to them 
+        const token = await sign({
+            id : response.email
+        } , c.env.JWT_SECRET) // it except first body as payload so that's why used this 
+
+        // used thier email as the json web token colud use thier id if i want to but used this and we have to update 
+        // thier json web token each time they signin or sing up or update thier passwrod 
+        // if update their password re-direct then to the sign in page 
+
+        
+        // item can not be set on the server side you can set on the client side only 
+
         return c.json({
             message : "User found",
-            user : response
+            user : response,
+            token : token
         },200)
     }
     catch (err) {
@@ -50,7 +66,7 @@ app.post('/signin', async (c) => {
 });
 
 
-app.post('/sigup', async (c) => {
+app.post('/signup', async (c) => {
     //wrapped all of this in the try cathc block
     try {
         const prisma = new PrismaClient({
@@ -72,20 +88,55 @@ app.post('/sigup', async (c) => {
             }
         })
 
+        // so giving the web token to them 
+        const token = await sign({
+            id : response.email
+        } , c.env.JWT_SECRET) // it except first body as payload so that's why used this 
+
+
         await prisma.$disconnect(); 
 
         return c.json({
             message: "User created succesfully",
-            user: response
+            user: response,
+            token : token
         },201);
     }
     catch (err) {
         return c.json({
             message: "Error while singup",
-            error: err
+            error: err,
         }, 500) // status code is send like that 
     }
 });
+
+
+// so now here we have to authinticate our application also 
+// so the way to use the authincation in hono to use the app.use
+app.use('/blog/*' , async(c , next) => {
+    // so now veryfying the logic here 
+    try{
+        const header = c.req.header("Authorization") || "";
+        // as the header will be with bearer and then the actual token so for that 
+        const token = header.split(" ")[1]; 
+
+        const respone = await verify(token , c.env.JWT_SECRET);
+        // so now we assing them with the id 
+        if(!respone.id){
+            return c.json({
+                message : "id is not defined"
+            },403)
+        }
+        await next(); // next call
+
+    }
+    catch(err){
+        return c.json({
+            message : "token experid or invalid user",
+            error : err
+        },500)
+    }
+})
 
 
 app.post('/blog', async (c) => {
