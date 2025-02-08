393b60d58f38fe0c790cd6f563a1e27d629e1a20
Ashok <ashokgupta2678@gmail.com>
2025-02-08 12:56:16 +0530

    changed the file sucture and added seperate routes and user and blog sepreately for clearner code

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,8 +1,12 @@
 import { Hono } from 'hono'
-import RouteApp from './Route'; // improting all my routes to use in the this file
+import UserRoute from './user'; // improting all my routes to use in the this file
+import BlogRoute from './blog';
+
 
 const app = new Hono()
-app.route('/api/v1' , RouteApp);
+
+app.route('/api/v1/user' , UserRoute); // routed all the api/v1/user calls to user.ts
+app.route('/api/v1/blog' , BlogRoute); // routed all the api/v1/blog calls to the blog.ts
 
 app.get('/', (c) => {
   return c.text('Hello Hono! without any route')
